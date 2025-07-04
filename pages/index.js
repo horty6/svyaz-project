3401172916908056809bf1d46ef79e7d0d628ebd
horty6 <hortytmlll@gmail.com>
2025-07-04 05:35:23 +0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div style={{ 
+      display: "flex", 
+      flexDirection: "column", 
+      alignItems: "center", 
+      justifyContent: "center", 
+      height: "100vh", 
+      fontFamily: "sans-serif" 
+    }}>
+      <h1>🔗 Svyaz Project</h1>
+      <p>Ты подключён. Это только начало. Добро пожаловать!</p>
+    </div>
+  );
+}
